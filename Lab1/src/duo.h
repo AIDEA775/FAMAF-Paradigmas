39567f414708c18d0b3862bcd5ce75d428c2dd01
Alee Silva <alex.sai95@gmail.com>
2016-04-01 12:00:13 -0300

    comments in duo

--- Lab1/src/duo.h
+++ Lab1/src/duo.h
@@ -11,12 +11,27 @@
 
 typedef struct _duo_t  *duo_t;
 
+/*
+ * Build a new pair from the given index and data.
+ * Can free index and data after creating the dou
+ */
 duo_t create_duo(char* index, char* data);
 
+/*
+ * Return data
+ */
 char* get_data(duo_t duo);
 
+/*
+ * Return index
+ */
 char* get_index(duo_t duo);
 
+/*
+ * Free the memory allocated by the given duo, as well as the respective
+ * index and data
+ * Return NULL
+ */
 duo_t destroy_duo(duo_t duo);
 
 #endif  // LAB1_SRC_DUO_H_
