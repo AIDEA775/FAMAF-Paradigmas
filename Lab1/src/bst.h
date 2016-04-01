4318302369d3aafedc5df1dbad2d158cb7dced78
Alee Silva <alex.sai95@gmail.com>
2016-04-01 12:20:28 -0300

    comments in bst

--- Lab1/src/bst.h
+++ Lab1/src/bst.h
@@ -12,12 +12,25 @@
 
 typedef struct _tree_node_t  *bst_t;
 
+/*
+ * Returns a newly created, empty binary search tree (BST)
+ */
 bst_t bst_empty(void);
 
+/*
+ * Returns the given bst with the pair index and data added to it
+ */
 bst_t bst_add(bst_t bst, char* index, char* data);
 
+/*
+ * Returns the data associated to the given index in the given bst,
+ * or NULL if the index is not in bst
+ */
 char* bst_search(bst_t bst, char* index);
 
+/*
+ * Free the resources allocated for the given bst, and return NULL
+ */
 bst_t bst_destroy(bst_t bst);
 
 #endif  // LAB1_SRC_BST_H_
