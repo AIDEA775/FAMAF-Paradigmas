a2144b5eac7051f2d644dddb1b679325726d00ba
Alejandro Nigrelli <alejandro.nigrelli@gmail.com>
2016-04-11 16:48:36 -0300

    Implemento salida de archivos

--- Lab2/src/com/unc/famaf/ales/SalidaArchivo.java
+++ Lab2/src/com/unc/famaf/ales/SalidaArchivo.java
@@ -1,5 +1,15 @@
 package com.unc.famaf.ales;
 
 public class SalidaArchivo {
-
+	FileWriter salida;
+	SalidaArchivo(String nombre){
+		try{
+			salida = new FileWriter(nombre);
+		}catch{
+			//poner la excepcion
+		}
+	}
+	public static void EscribirArchivo(String palabra){
+		salida.write(palabra);
+	}
 }
