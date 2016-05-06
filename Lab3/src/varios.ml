3b33fe04b3048c166f4bc1f468e1ee0dfa8ff783
Alejandro Ismael Silva <alex.sai95@gmail.com>
2016-05-06 13:09:10 -0300

    implementando varios

--- Lab3/src/varios.ml
+++ Lab3/src/varios.ml
@@ -1,3 +1,19 @@
-val leer_letra : unit -> char
+let rec leer_letra unit : char =
+  let s = read_line() in
+  match String.length s with
+  | 0 -> leer_letra()
+  | _ -> s.[0]
 
-val leer_palabra : unit -> string
+
+let rec leer_palabra unit : string =
+  let s = read_line() in
+  match String.length s with
+  | 0 -> leer_palabra()
+  | _ -> s
+
+
+(* debug *)
+let () =
+  let open Printf in
+  printf ">>> Palabra: %s\n" (leer_palabra () );
+  printf ">>> Letra: %c\n" (leer_letra() );;
