d7ae96ed8d28bd65c0010c61d8b40fac60525e9d
Alejandro Ismael Silva <alex.sai95@gmail.com>
2016-05-07 02:30:54 -0300

    errores de sintaxis muy feos por no saber bien la sintaxis

--- Lab3/src/mesa.ml
+++ Lab3/src/mesa.ml
@@ -1,40 +1,75 @@
 open Jugadores
 open Cartas
 
-type mesa = {jugadores : jugador list; mazo : cartas list};;
-
-let rec cargar_jugadores (i : int) : jugador list =
-  match i with
-  | 0 -> []
-  | _ ->  let nombre = leer_palabra() in
-          match nombre with
-          | "EXIT" -> []
-          | _ ->  let j = crear_jugador nombre in
-                  j :: cargar_jugadores (i-1);;
-
+type mesa = {jugadores : jugador list; mazo : cartas};;
 
 let crear_mesa unit : mesa =
-  let j = cargar_jugadores 5 in
-  let c = mazo_completo() in
-  {jugadores=j; mazo=c};;
+  (* carga y devuelve jugadores y las cartas que sobran *)
+  let rec cargar_jugadores (i : int) (c : cartas) : jugador list * cartas =
+    match i with
+    | 0 -> []
+    | _ ->  let nombre = leer_palabra() in
+            match nombre with
+            | "EXIT" -> []
+            | _ ->  let j, c = crear_jugador nombre c in
+                    let js, c = cargar_jugadores (i-1) c in
+                    (j :: js, c)
+  in
+  let j, c = cargar_jugadores 5 mazo_completo() in
+  {jugadores = j; mazo = c};;
 
-let jugar_ronda (m : mesa) : mesa =;;
+(*falta imprimir cosas :P*)
+let jugar_ronda (m : mesa) : mesa =
+  (* juega una ronda y devuelve los nuevos jugadores y el nuevo mazo *)
+  let rec jugar (js : jugador list) (cs : cartas) : jugador list * cartas =
+    match js with
+    | [] -> ([], cs)
+    | x::xs ->  let nuevo_j, cs = jugador_juega x cs in
+                let nuevos_js, cs = jugar xs cs in
+                (nuevo_j :: nuevos_js, cs)
+  in
+  let js, c = jugar m.jugadores m.mazo in
+  {m with jugadores = js; mazo = c};;
 
-let ganador_ronda (m : mesa) : jugador =
-  ;;
+let ganador_ronda (m : mesa) : mesa * jugador =
+  (* crear lista de cartas jugadas *)
+  let rec cartas_jugadas (js : jugador list) : cartas =
+    match js with
+    | [] -> []
+    | x::xs -> (jugador_carta_jugada x) :: cartas_jugadas xs
+  in
+  (* devuelve el jugador que jugó esa carta*)
+  let rec buscar_jugador (js : jugador list) (carta : carta) : jugador =
+    match js with
+    | [x] -> x (* tiene que ser *)
+    | x::xs -> if (jugador_carta_jugada x) = carta then x else buscar_jugador xs carta
+  in
+  (* actualiza los jugadores sumando un punto a j*)
+  let rec actualizar (js : jugador list) (j : jugador) : jugador list =
+    match js with
+    | [x] -> jugador_suma_punto x (*tiene que ser*)
+    | x::xs -> if x = j then (jugador_suma_punto x) :: xs else x :: (actualizar js j)
+  in
+  let carta = carta_maxima (cartas_jugadas m.jugadores) in
+  let j = buscar_jugador m.jugadores carta in
+  ({m with jugadores = actualizar m.jugadores j}, (jugador_suma_punto j));;
 
 let limpiar_mesa (m : mesa) : mesa =
   (*quitar carta jugada*)
-  let rec limpiar (js : jugador list) : jugador list = function
+  let rec limpiar (js : jugador list) : jugador list =
+    match js with
     | [] -> []
-    | x::xs -> (jugador_limpiar_carta x) :: limpiar xs;;
-  {m with jugadores=limpiar m.jugadores};;
+    | x::xs -> (jugador_limpiar_carta x) :: limpiar xs
+  in
+  {m with jugadores = limpiar m.jugadores};;
 
 let cantidad_jugadores (m : mesa) : int =
   (*contar la cantidad de jugadores jugando*)
-  let rec contar (js : jugadores list) : int = function
+  let rec contar (js : jugadores list) : int =
+    match js with
     | [] -> 0
-    | x::xs -> if jugador_quedan_cartas x then 1 + contar xs else contar xs;;
+    | x::xs -> if jugador_quedan_cartas x then 1 + contar xs else contar xs
+  in
   contar m.jugadores;;
 
 let () =
--- Lab3/src/mesa.mli
+++ Lab3/src/mesa.mli
@@ -4,8 +4,8 @@ val crear_mesa : unit -> mesa
 
 val jugar_ronda : mesa -> mesa
 
-val ganador_ronda : mesa -> jugador
+val ganador_ronda : mesa -> mesa * jugador
 
-val limpiar_mesa : mesa -> mesa 
+val limpiar_mesa : mesa -> mesa
 
 val cantidad_jugadores : mesa -> int
