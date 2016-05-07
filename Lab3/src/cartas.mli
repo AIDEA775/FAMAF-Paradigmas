00775ba13625684a2e3d60a76acc0686fdfbff41
Alejandro Ismael Silva <alex.sai95@gmail.com>
2016-05-06 22:42:08 -0300

    nigrelli se sentirá feliz al ver una especificacion inambigua

--- Lab3/src/cartas.mli
+++ Lab3/src/cartas.mli
@@ -2,26 +2,42 @@ type cartas
 
 type carta
 
+(* (a) devuelve un mazo de cartas vacio
+   (b) crea un mazo de 7 cartas para un jugador
+  con (a) jugador deberia generar su mazo
+  con (b) deberia pasarte el mazo general para que saques cartas de ahí
+  decidí *)
 val crear_mazo : unit -> cartas
 
+(* devuelve el mazo entero mezclado *)
 val mazo_completo : unit -> cartas
 
-val sacar_cartas : cartas -> cartas -> cartas
-
-val poner_cartas : cartas -> cartas -> cartas
-
-val imprimir_mazo : cartas -> unit
-
+(* devuelve la carta segun un string *)
 val string_a_carta : string -> carta
 
+(* saca cartas de un mazo y devuelve el mazo restante
+  mazo -> cartas a sacar -> resultado *)
+val sacar_cartas : cartas -> cartas -> cartas
+
+(* si pone las cartas al ultimo no seria necesario "poner_ultimo"
+  mazo -> cartas a poner -> resultado *)
+val poner_cartas : cartas -> cartas -> cartas
+
+(* pone una carta al final del mazo
+   mazo -> carta a poner -> resultado *)
+val poner_ultimo : cartas -> carta -> cartas
+
+(* devuelve la primera carta del mazo *)
+val primer_carta : cartas -> carta
+
+(* imprimir por std las cartas separadas por un espacio *)
+val imprimir_mazo : cartas -> unit
+
+(* devuelve la carta mas grande *)
 val carta_maxima : cartas -> carta
 
+(* devuelve la carta mas chica *)
 val carta_minima : cartas -> carta
 
-val poner_ultimo : carta -> cartas -> cartas
-
-val primer_carta : cartas -> carta  
-
+(* devuelve las cartas que son pares *)
 val cartas_pares : cartas -> cartas
-
-
