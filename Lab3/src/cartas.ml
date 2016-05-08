448f95fae12377252c225d00e842befdc576a7d9
Alejandro Ismael Silva <alex.sai95@gmail.com>
2016-05-08 16:05:39 -0300

    ya casi estamos

--- Lab3/src/cartas.ml
+++ Lab3/src/cartas.ml
@@ -8,14 +8,6 @@ let mazo = [("E",1);("E",2);("E",3);("E",4);("E",5);("E",6);("E",7);("E",8);("E"
            ;("O",10);("O",11);("O",12);("C",1);("C",2);("C",3);("C",4);("C",5)
            ;("C",6);("C",7);("C",8);("C",9);("C",10);("C",11);("C",12)];;
 
-(*funcion auxiliar para crear mazo*)
-let split list n =
-    let rec aux i acc = function
-      | [] -> List.rev acc, []
-      | h :: t as l -> if i = 0 then List.rev acc, l
-                       else aux (i-1) (h :: acc) t  in
-    aux n [] list;;
-
 let crear_mazo (m : cartas) : cartas =
   let rec tomar (m : cartas) (i : int) : cartas =
     match i with
@@ -42,43 +34,16 @@ let crear_mazo (m : cartas) : cartas =
      aux [] mazo (List.length mazo)
  ;;
 
-(*funciones auxiliares para string_a_carta in_of_string()*)
-  let rec string_a_lista carta = match carta with
-    | "" -> []
-    | carta -> (String.get carta 0 ) :: (string_a_lista (String.sub carta 1 ( (String.length carta)-1) ) )
-;;
-
-let list_a_string cl = String.concat "" (List.map (String.make 1) cl) ;;
-
-let convertirla lista =
-  let carta = split lista 1 in
-  let tipo,numero = carta in
-  let tipo = list_a_string(tipo) in
-  let numero = int_of_string(list_a_string(numero)) in
-  let carta = tipo,numero in
-  carta
-;;
-
-let cartas_iguales carta1 carta2 =
-  let x,y = carta1 in
-  let w,z = carta2 in
-  let rbool = x = w && y = z in
-  rbool
-;;
-
-let rec la_carta_esta cartas carta =
-  match cartas with
-  |[]  -> false
-  | x :: cartas -> cartas_iguales x carta  || la_carta_esta cartas carta
-;;
-
 let carta_of_string (cs : cartas) (s : string) : carta option =
-  let lista = string_a_lista c in
-  let cartaaux = convertirla(lista) in
-  (*nose que poner acaaa!!!! :S !!!!!!!! por todo tira bardo*)
-  cartaaux
-;;
-
+  if String.length s < 2 || not (List.exists (fun (t,n) -> t = (String.sub s 0 1)) cs)
+    then None
+  else
+    try
+      let n = String.sub s 1 ((String.length s) - 1) in
+      let n = int_of_string n in
+      if not (List.exists (fun (t,i) -> i = n) cs) then None else Some (String.sub s 0 1, n)
+    with
+      | Failure "int_of_string" -> None;;
 
 let string_of_carta (c : carta) : string =
   let x, y = c in
@@ -117,24 +82,17 @@ let primera_carta (c : cartas) : carta option =
   | [] -> None
   | c::cs -> Some c;;
 
-(*auxiliar para carta_maxima y carta_minima (compara -> carta1 < carta2) *)
-let valor_carta (c : carta) : int =
-  let t,n = c in
-  match t with
-  | "E" -> 1000
-  | "B" -> 100
-  | "O" -> 10
-  | "C" -> 1
-;;
-
 let comparar_carta (c: carta) (t : carta) : bool =
-  let num_c = snd c in
-  let num_t = snd t in 
-  let valor_c = valor_carta c in
-  let valor_t = valor_carta t in
-  valort_c*c < valor_t*t
-;; 
-
+  let valor_carta (c : carta) : int =
+    let t,n = c in
+    match t with
+    | "E" -> 1000 * n
+    | "B" -> 100 * n
+    | "O" -> 10 * n
+    | "C" -> 1 * n
+    | _ -> 0
+  in
+  valor_carta c < valor_carta t;;
 
 let rec carta_maxima (cartas :cartas) : carta option  =
    match cartas with
