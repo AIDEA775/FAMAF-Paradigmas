fd68626bfffc9c02841c9837ab045875ac4b42d1
alejandro <alejandro.nigrelli@gmail.com>
2016-05-08 14:37:35 -0300

    Seguimos arreglando y el tiempo nos corre

---
+++ Lab3/Readme.md
@@ -0,0 +1,15 @@
+guerra funcional en OCAML
+
+implementacion
+
+cartas
+
+jugador
+
+mesa
+
+guerra funcional
+
+decisiones
+
+ideas
--- Lab3/src/cartas.ml
+++ Lab3/src/cartas.ml
@@ -1,5 +1,12 @@
 type carta = string * int
+type cartas = carta list
 
+let mazo = [("E",1);("E",2);("E",3);("E",4);("E",5);("E",6);("E",7);("E",8);("E",9)
+           ;("E",10);("E",11);("E",12);("B",1);("B",2);("B",3);("B",4);("B",5)
+           ;("B",6);("B",7);("B",8);("B",9);("B",10);("B",11);("B",12);("O",1)
+           ;("O",2);("O",3);("O",4);("O",5);("O",6);("O",7);("O",8);("O",9)
+           ;("O",10);("O",11);("O",12);("C",1);("C",2);("C",3);("C",4);("C",5)
+           ;("C",6);("C",7);("C",8);("C",9);("C",10);("C",11);("C",12)];;
 (*funcion auxiliar para crear mazo*)
 let split list n =
     let rec aux i acc = function
@@ -9,38 +16,30 @@ let split list n =
     aux n [] list;;
 
 let crear_mazo (m : cartas) : cartas =
-  let rec tomar (cs : cartas) (i : int) : cartas =
+  let rec tomar (m : cartas) (i : int) : cartas =
     match i with
     | 0 -> []
-    | _ ->  match cs with
+    | _ ->  match m with
             | [] -> []
-            | x::xs -> x :: tomar xs i-1
+            | x::xs -> x :: tomar xs (i-1)
   in
-  tomar cs 7;;
+  tomar m 7;;
 
-let mazo_completo unit : cartas = [("E",1);("E",2);("E",3);("E",4);("E",5);("E",6);("E",7);("E",8);("E",9)
-                                  ;("E",10);("E",11);("E",12);("B",1);("B",2);("B",3);("B",4);("B",5)
-                                  ;("B",6);("B",7);("B",8);("B",9);("B",10);("B",11);("B",12);("O",1)
-                                  ;("O",2);("O",3);("O",4);("O",5);("O",6);("O",7);("O",8);("O",9)
-                                  ;("O",10);("O",11);("O",12);("C",1);("C",2);("C",3);("C",4);("C",5)
-                                  ;("C",6);("C",7);("C",8);("C",9);("C",10);("C",11);("C",12)];;
-
-(*
-    let rec extraer (acc : cartas) (n : int) (mazo : cartas): carta * cartas =
-      match mazo with
-      | [] -> raise Not_found
-      | h :: t -> if n = 0 then (h, acc @ t) else extraer (h::acc) (n-1) t
-    in
-    let extraer_aleatorio (mazo : cartas) (len : int) : carta * cartas =
-      extraer [] (Random.int len) mazo
-    in
-    let rec aux (acc : cartas) (mazo : cartas) (len : int) : cartas =
-      if len = 0 then acc else
-        let picked, rest = extraer_aleatorio mazo len in
-        aux (picked :: acc) rest (len-1)
-    in
-    aux [] mazo_lista (List.length mazo_lista)
-;;*)
+ let rec mazo_completo unit =
+     let rec extraer acc n = function
+       | [] -> raise Not_found
+       | h :: t -> if n = 0 then (h, acc @ t) else extraer (h::acc) (n-1) t
+     in
+     let extraer_aleatorio mazo len =
+       extraer [] (Random.int len) mazo
+     in
+     let rec aux acc mazo len =
+       if len = 0 then acc else
+         let picked, rest = extraer_aleatorio mazo len in
+         aux (picked :: acc) rest (len-1)
+     in
+     aux [] mazo (List.length mazo)
+ ;;
 
 (*funciones auxiliares para string_a_carta in_of_string()*)
   let rec string_a_lista carta = match carta with
@@ -81,7 +80,7 @@ let carta_of_string (cs : cartas) (c : string) : carta =
 
 
 let string_of_carta (c : carta) : string =
-  let x, y = carta in
+  let x, y = c in
   let y = string_of_int y in
   x ^ y;;
 
@@ -128,8 +127,9 @@ let comparar_carta carta1 carta2 =
    |("c",_) -> w = "e" || w = "b" || w = "o" || w = "c" && y <= z
 ;;
 
-let rec carta_maxima cartas =
+let rec carta_maxima (cartas :cartas) : carta option  =
    match cartas with
+   |[] -> None
    | h :: [] -> h
    | h :: cartas ->
    let aux = carta_maxima cartas in
